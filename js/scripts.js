1fdb854e97efc2d518ee25a07001a500d716d3cf
Evan Hadfield and Marc Davies <marc@>
2019-04-01 09:58:59 -0700

    add html and script to fill empty array with javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 $(function() {
-
+  $("#userinput").submit(function(event){
+   var favoritething1 = $("#favoritething1").val();
+   var favoritething2 = $("#favoritething2").val();
+   var favoritething3 = $("#favoritething3").val();
+   console.log(favoritething1);
+   console.log(favoritething2);
+   console.log(favoritething3);
+   var favorites = [favoritething1, favoritething2, favoritething3];
+   //favorites.fill(favoritething1, favoritething2, favoritething3);
+   console.log(favorites);
+   event.preventDefault();
+  });
 });
